187eb8827e8a8cba4103e39a3e2c4623a6599952
nadunz <nadunp94@gmail.com>
2020-07-02 12:46:32 +0530

    Initiate project

---
+++ src/main/java/com/haulmatic/roleapi/RoleapiApplication.java
@@ -0,0 +1,13 @@
+package com.haulmatic.roleapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoleapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RoleapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/haulmatic/roleapi/RoleapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haulmatic.roleapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoleapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
